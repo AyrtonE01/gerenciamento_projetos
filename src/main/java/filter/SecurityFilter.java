b34424adf9a0c5e4f23694061f7e045f6b494c96
AyrtonE01 <ayrton.enokida123@gmail.com>
2026-05-25 22:06:16 -0300

    Ajuste do SecurityFilter

--- src/main/java/filter/SecurityFilter.java
+++ src/main/java/filter/SecurityFilter.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityFilter {
 
     @Bean
